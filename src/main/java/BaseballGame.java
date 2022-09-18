13b8083eeffd777e8128ee2e70238941a1aabaf1
jaejun1130 <wjswowns1234@gmail.com>
2022-09-18 09:46:26 +0900

    feat: 숫자 비교 메소드 작성

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -20,4 +20,25 @@ public class BaseballGame {
         }
         return computerNumber;
     }
+
+    public int total(List<Integer> computer, List<Integer> player){
+        int total = 0;
+        for(int i = 0; i < player.size(); i++){
+            if(computer.contains(player.get(i))){
+                total += 1;
+            }
+        }
+        return total;
+    }
+
+
+    public int strike(List<Integer> computer, List<Integer> player){
+        int strike = 0;
+        for(int i = 0; i < player.size(); i++){
+            if(computer.get(i).equals(player.get(i))){
+                strike += 1;
+            }
+        }
+        return strike;
+    }
 }
