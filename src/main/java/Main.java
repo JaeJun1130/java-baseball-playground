5c1ee0132866ff37f162038512f045e8114cccaa
jaejun1130 <wjswowns1234@gmail.com>
2022-09-18 19:59:32 +0900

    feat: main 메소드 작성

---
+++ src/main/java/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        BaseballGame baseballGame = new BaseballGame();
+        boolean retry = true;
+
+        while (retry) {
+            String result = "";
+            List<Integer> computer = baseballGame.create();
+            while (!result.equals("3스트라이크")) {
+                System.out.println("숫자를 입력해주세요");
+                Scanner scanner = new Scanner(System.in);
+                List<Integer> player = new ArrayList<>();
+                String input = scanner.next();
+
+                for (String number : input.split("")) {
+                    player.add(Integer.parseInt(number));
+                }
+
+                int total = baseballGame.total(computer, player);
+                int strike = baseballGame.strike(computer, player);
+                int ball = total - strike;
+
+                if (total == 0) {
+                    result = "낫싱";
+                } else if (strike == 0) {
+                    result = ball + "볼";
+                } else if (ball == 0) {
+                    result = strike + "스트라이크";
+                } else {
+                    result = ball + "볼 " + strike + "스트라이크";
+                }
+
+                System.out.println(result);
+            }
+            retry = baseballGame.retry();
+        }
+    }
+}
