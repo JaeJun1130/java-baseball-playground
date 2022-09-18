d24e05c1e18180b21517f6df36abf522b0b727c5
jaejun1130 <wjswowns1234@gmail.com>
2022-09-18 19:59:13 +0900

    feat: 재시작 판별여부 메소드 생성

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,30 +1,31 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class BaseballGame {
-    public int random(){
+    public int random() {
         Random random = new Random();
         return random.nextInt(9) + 1;
     }
 
-    public List<Integer> create(){
+    public List<Integer> create() {
         List<Integer> computerNumber = new ArrayList<>();
-        while (computerNumber.size() < 3){
+        while (computerNumber.size() < 3) {
             int randomNumber = random();
-            if(computerNumber.contains(randomNumber)){
+            if (computerNumber.contains(randomNumber)) {
                 continue;
-            }else {
+            } else {
                 computerNumber.add(randomNumber);
             }
         }
         return computerNumber;
     }
 
-    public int total(List<Integer> computer, List<Integer> player){
+    public int total(List<Integer> computer, List<Integer> player) {
         int total = 0;
-        for(int i = 0; i < player.size(); i++){
-            if(computer.contains(player.get(i))){
+        for (int i = 0; i < player.size(); i++) {
+            if (computer.contains(player.get(i))) {
                 total += 1;
             }
         }
@@ -32,13 +33,23 @@ public class BaseballGame {
     }
 
 
-    public int strike(List<Integer> computer, List<Integer> player){
+    public int strike(List<Integer> computer, List<Integer> player) {
         int strike = 0;
-        for(int i = 0; i < player.size(); i++){
-            if(computer.get(i).equals(player.get(i))){
+        for (int i = 0; i < player.size(); i++) {
+            if (computer.get(i).equals(player.get(i))) {
                 strike += 1;
             }
         }
         return strike;
+    }
+
+    public boolean retry() {
+        System.out.println("축하합니다! 경기를 다시 시작하겠습니까? 다시 시작 : 1, 종료 : 2");
+        Scanner scanner = new Scanner(System.in);
+        char c = scanner.next().charAt(0);
+        if (c == '1') {
+            return true;
+        }
+        return false;
     }
 }
